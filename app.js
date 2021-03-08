77265402a4a64c5828f42ea3a8a25ab4a554b269
kylehansen17 <kyle.hansen925@gmail.com>
2021-03-08 08:03:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ for (let link of allLinks) {
     link.style.textDecorationColor = 'magenta';
     link.style.textDecorationStyle = 'wavy'
 }
+
+
+// Select all spans, iterate over them, and assign them each one of the
+// colors from the colors array
+// const colors = ['red', 'orange', 'yellow', 'green'];
+// const span = document.querySelectorAll('span');
+
+// for (let i = 0; i < span.length; i++) {
+//     span[i].style.color = color[i];
+// }
